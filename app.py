5643a78682ac8a77569aa51d6efb50478d974b10
pedroalanisdiez <juanpedroalanisdiez@gmail.com>
2021-11-11 09:22:18 -0300

    version 1

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template, redirect
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/tasks.db"
+db = SQLAlchemy(app)
+
+
+#Creamos una clase para modelar datos.
+#Dentro de task creamos una instancia de la conexion db.
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    tasks  = Task.query.all()
+    return render_template('index.html', tasks = tasks)
+
+@app.route('/create-task', methods=['POST'])
+def create():
+    task = Task(content = request.form['content'], done = False)
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
